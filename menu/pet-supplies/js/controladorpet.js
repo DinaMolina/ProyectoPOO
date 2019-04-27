45f82a27b83eeefd381185e395f9faf965a77d6a
BryanJCuellar <bryanb194@yahoo.com>
2019-04-27 17:24:46 -0600

    Commit

--- menu/pet-supplies/js/controladorpet.js
+++ menu/pet-supplies/js/controladorpet.js
@@ -3,15 +3,15 @@ $(document).ready(function(){
 });
 
 var buyitems=[
-    {categoria:"PetSupplies",subcategoria:"Principal",caratula:"img/Heartdisease.jpg",texto:"Prevent and Reverse Heart Disease: The Revolutionary, Scientifically Proven, Nutrition-Based Cure",eligible:true,numero:11,centavo:55,producto:"Heart Disease Cure"},
-    {categoria:"PetSupplies",subcategoria:"Principal",caratula:"img/Biscuitpet.jpg",texto:"Biscuit's Pet & Play Easter: A Touch & Feel Book",eligible:true,numero:7,centavo:48,producto:"Biscuit's Pet & Play Easter: A Touch & Feel Book"},
-    {categoria:"PetSupplies",subcategoria:"Principal",caratula:"img/Xaviersolutions.jpg",texto:"Xavier Training Solutions Stake and Tie Out Combo for Dogs | 20ft. Cable Leash Pet Tether System Blue | 18' Spiral Stake",eligible:true,numero:15,centavo:50,producto:"20ft. Cable Leash Pet Tether System Blue"},
-    {categoria:"PetSupplies",subcategoria:"Principal",caratula:"img/Dogzilla.jpg",texto:"Dogzilla (digest)",eligible:true,numero:5,centavo:95,producto:"Dogzilla (digest)"},
-    {categoria:"PetSupplies",subcategoria:"Principal",caratula:"img/Puppytricks.jpg",texto:"51 Puppy Tricks: Step-by-Step Activities to Engage",eligible:true,numero:8,centavo:47,producto:"51 Puppy Tricks"},
-    {categoria:"PetSupplies",subcategoria:"Principal",caratula:"img/Thinklikecat.jpg",texto:"Think Like a Cat: How to Raise a Well-Adjusted Cat",eligible:true,numero:14,centavo:65,producto:"Think Like a Cat: How to Raise a Well-Adjusted Cat"},
-    {categoria:"PetSupplies",subcategoria:"Principal",caratula:"img/Hotelfordogs.jpg",texto:"Hotel For Dogs",eligible:true,numero:7,centavo:79,producto:"Hotel For Dogs"},
-    {categoria:"PetSupplies",subcategoria:"Principal",caratula:"img/Dogpurpose.jpg",texto:"A Dog's Purpose: A Novel for Humans",eligible:true,numero:8,centavo:76,producto:"A Dog's Purpose: A Novel for Humans"},
-    {categoria:"PetSupplies",subcategoria:"Principal",caratula:"img/Kavik.jpg",texto:"Kavik the Wolf Dog",eligible:true,numero:6,centavo:99,producto:"Kavik the Wolf Dog"},
+    {categoria:"PetSupplies",subcategoria:"Principal",caratula:"img/Heartdisease.jpg",texto:"Prevent and Reverse Heart Disease: The Revolutionary, Scientifically Proven, Nutrition-Based Cure",eligible:true,numero:11,centavo:55,precio:"$11.55",producto:"Heart Disease Cure"},
+    {categoria:"PetSupplies",subcategoria:"Principal",caratula:"img/Biscuitpet.jpg",texto:"Biscuit's Pet & Play Easter: A Touch & Feel Book",eligible:true,numero:7,centavo:48,precio:"$7.48",producto:"Biscuit's Pet & Play Easter: A Touch & Feel Book"},
+    {categoria:"PetSupplies",subcategoria:"Principal",caratula:"img/Xaviersolutions.jpg",texto:"Xavier Training Solutions Stake and Tie Out Combo for Dogs | 20ft. Cable Leash Pet Tether System Blue | 18' Spiral Stake",eligible:true,numero:15,centavo:50,precio:"$15.50",producto:"20ft. Cable Leash Pet Tether System Blue"},
+    {categoria:"PetSupplies",subcategoria:"Principal",caratula:"img/Dogzilla.jpg",texto:"Dogzilla (digest)",eligible:true,numero:5,centavo:95,precio:"$5.95",producto:"Dogzilla (digest)"},
+    {categoria:"PetSupplies",subcategoria:"Principal",caratula:"img/Puppytricks.jpg",texto:"51 Puppy Tricks: Step-by-Step Activities to Engage",eligible:true,numero:8,centavo:47,precio:"$8.47",producto:"51 Puppy Tricks"},
+    {categoria:"PetSupplies",subcategoria:"Principal",caratula:"img/Thinklikecat.jpg",texto:"Think Like a Cat: How to Raise a Well-Adjusted Cat",eligible:true,numero:14,centavo:65,precio:"$14.65",producto:"Think Like a Cat: How to Raise a Well-Adjusted Cat"},
+    {categoria:"PetSupplies",subcategoria:"Principal",caratula:"img/Hotelfordogs.jpg",texto:"Hotel For Dogs",eligible:true,numero:7,centavo:79,precio:"$7.79",producto:"Hotel For Dogs"},
+    {categoria:"PetSupplies",subcategoria:"Principal",caratula:"img/Dogpurpose.jpg",texto:"A Dog's Purpose: A Novel for Humans",eligible:true,numero:8,centavo:76,precio:"$8.76",producto:"A Dog's Purpose: A Novel for Humans"},
+    {categoria:"PetSupplies",subcategoria:"Principal",caratula:"img/Kavik.jpg",texto:"Kavik the Wolf Dog",eligible:true,numero:6,centavo:99,precio:"$6.99",producto:"Kavik the Wolf Dog"},
 
 ]
 
