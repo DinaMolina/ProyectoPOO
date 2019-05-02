1857b76a71fd88b00950724f88af44265abdf642
BryanJCuellar <bryanb194@yahoo.com>
2019-05-01 22:48:50 -0600

    Commit

---
+++ menu/Arts & Crafts/js/controlador.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+    menu();
+    desplegarLista1();
+    desplegaridiomas();
+});
+
+var departamentos=[    
+    {area:'Prime Video'},{area:'Music, CDs & Vinyl'},{area:'Digital Musical'},{area:'Kindle Store'},{area:'Arts & Crafts'},{area:'Automotive'},{area:'Baby'},
+    {area:'Beauty & Personal Care'},{area:'Book'},{area:'Computers'},{area:'Electronics'},{area:"Women's Fashion"},{area:"Men's Fashion"},{area:'Health & Household'},
+    {area:'Home and kitchen'},{area:'Industrys & Scientific'},{area:'Luggage'},{area:'Movies & Televisor'},{area:'Pet Supplies'},{area:'Software'},{area:'Sports & Outdoors'},
+    {area:'Tools & Home Improvement'},{area:'Toys & Games'},{area:'Video Games'},{area:'Deals'},
+];
+
+var lista1=[
+    {opcion:''},{opcion:'Your Lists'},{opcion:'Create a List'},{opcion:'Find a List or Registry'},{opcion:'Find a Gift'},{opcion:'Save Items from the Web'},{opcion:'Wedding Registry'},{opcion:'Baby Registry'},
+    {opcion:'Friends & Family Gifting'},{opcion:'AmazonSmile Charity Lists'},{opcion:'Pantry Lists'},{opcion:'Your Hearts'},
+    {opcion:'Explore Idea Lists'},{opcion:'Explore Showroom'},{opcion:'Take the Home Style '},
+];
+
+var idiomas=[
+    {idioma:''},{idioma:'English'},{idioma:'Español'},{idioma:'Portugues'},{idioma:'简体中文'},{idioma:'Deutsch - DE'},{idioma:'繁體中文 - ZH'},
+];
+
+var productos=[{titulo:'Iggy Peck, Architec', imagen:'img7/4.jpg', precio:'15.50'  },{titulo:'School Zone - Preschool Basics Workbook, 64 Pages, Ages 3 to 5, Colors, Numbers, Counting, Matching, Classifying, Beginning Sounds, and More (School Zone Basics Workbook Series', imagen:'img7/5.jpg', precio:'15.50'  },
+{titulo:'Origami Paper 500 sheets Kaleidoscope Patterns 6" (15 cm): Tuttle Origami Paper: High-Quality Origami Sheets Printed with 12 Different Designs: Instructions for 8 Projects Included', imagen:'img7/6.jpg', precio:'15.50'  },{titulo:'Rock Art Handbook: Techniques and Projects for Painting, Coloring, and Transforming Stones (Fox Chapel Publishing) Over 30 Step-by-Step Tutorials using Paints, Chalk, Art Pens, Glitter Glue & More', imagen:'img7/7.jpg', precio:'15.50'  },
+{titulo:'Vogue® Knitting The Ultimate Knitting Book: Completely Revised & Updated', imagen:'img7/8.jpg', precio:'15.50'  },{titulo:"Carol Doak's Foundation Paper", imagen:'img7/9.jpg', precio:'15.50'  },
+{titulo:'My First Origami Kit: [Origami Kit with Book, 60 Papers, 150 Stickers, 20 Projects]', imagen:'img7/10.jpg', precio:'15.50'  },{titulo:'Sewing School ®: 21 Sewing Projects Kids Will Love to Make', imagen:'img7/11.jpg', precio:'15.50'  },
+{titulo:'Moda All-Stars - On a Roll: 14 Quilts That Start with 2 1/2" Strips', imagen:'img7/12.jpg', precio:'15.50'  },{titulo:'', imagen:'img7/13.jpg', precio:'15.50'  },
+{titulo:'Friendship Bracelets 101: Fun to Make, Fun to Wear, Fun to Share (Can Do Crafts) (Design Originals) Step-by-Step Instructions; Colorful Knotted Bracelets Made with Embroidery Floss for Kids & Teens', imagen:'img7/14.jpg', precio:'15.50'  },{titulo:'Bob Ross Bobblehead: With Sound! (Miniature Editions)', imagen:'img7/15.jpg', precio:'15.50'  },
+{titulo:'Mandalas II Adult Coloring Book - Features 50 Original Hand Drawn Designs Printed on Artist Quality Paper with Hardback Covers, Top Spiral Binding, Perforated Pages, and Bonus Blotter by ColorIt', imagen:'img7/16.jpg', precio:'15.50'  }
+];
+
+function menu(){
+    $('#menu').empty();
+    for(var i=0; i<departamentos.length; i++){
+       $('#menu').append(`
+       <a class="dropdown-item" href="menu/${departamentos[i].area}/${departamentos[i].area}.html">
+         ${departamentos[i].area}
+       </a><br>
+        `);
+    }
+}
+
+function desplegarLista1(){
+    $('#lista').empty();
+    for(var i=0; i<lista1.length; i++){
+       $('#lista').append(`<option value=${i}>
+       <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         ${lista1[i].opcion}
+       </a>
+   </option>`);
+    }
+}
+
+function desplegaridiomas(){
+    $('#idioma').empty();
+    for(var i=0; i<idiomas.length; i++){
+       $('#idioma').append(`<option value=${i}>
+       <label>${idiomas[i].idioma}<input type="radio" value=${idiomas[i].idioma}>
+       </label>
+       </option>` );
+    }
+}
+
+
+
+
+
+       
---
+++ menu/Pet Supplies/js/controladorpet.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+    generarListaPrincipalBuyItems();
+});
+
+var buyitems=[
+    {departamento:"PetSupplies",categoria:"Principal",imagen:"img/Heartdisease.jpg",producto:"Prevent and Reverse Heart Disease: The Revolutionary, Scientifically Proven, Nutrition-Based Cure",eligible:true,numero:11,centavo:55,precio:"$11.55"},
+    {departamento:"PetSupplies",categoria:"Principal",imagen:"img/Biscuitpet.jpg",producto:"Biscuit's Pet & Play Easter: A Touch & Feel Book",eligible:true,numero:7,centavo:48,precio:"$7.48"},
+    {departamento:"PetSupplies",categoria:"Principal",imagen:"img/Xaviersolutions.jpg",producto:"Xavier Training Solutions Stake and Tie Out Combo for Dogs",eligible:true,numero:15,centavo:50,precio:"$15.50"},
+    {departamento:"PetSupplies",categoria:"Principal",imagen:"img/Dogzilla.jpg",producto:"Dogzilla (digest)",eligible:true,numero:5,centavo:95,precio:"$5.95"},
+    {departamento:"PetSupplies",categoria:"Principal",imagen:"img/Puppytricks.jpg",producto:"51 Puppy Tricks: Step-by-Step Activities to Engage",eligible:true,numero:8,centavo:47,precio:"$8.47"},
+    {departamento:"PetSupplies",categoria:"Principal",imagen:"img/Thinklikecat.jpg",producto:"Think Like a Cat: How to Raise a Well-Adjusted Cat",eligible:true,numero:14,centavo:65,precio:"$14.65"},
+    {departamento:"PetSupplies",categoria:"Principal",imagen:"img/Hotelfordogs.jpg",producto:"Hotel For Dogs",eligible:true,numero:7,centavo:79,precio:"$7.79"},
+    {departamento:"PetSupplies",categoria:"Principal",imagen:"img/Dogpurpose.jpg",producto:"A Dog's Purpose: A Novel for Humans",eligible:true,numero:8,centavo:76,precio:"$8.76"},
+    {departamento:"PetSupplies",categoria:"Principal",imagen:"img/Kavik.jpg",producto:"Kavik the Wolf Dog",eligible:true,numero:6,centavo:99,precio:"$6.99"},
+
+]
+
+function generarListaPrincipalBuyItems(){
+    $('#petItems').empty();
+    for(var i=0; i<buyitems.length; i++){
+        if(buyitems[i].categoria=="Principal"){
+            if(buyitems[i].eligible){
+                buyitems[i].eligible="Eligible for Shipping to Honduras";
+                $("#petItems").append(
+                    `
+                    <div class="td-petitem col-xl-4 col-md-6 col-12">
+                        <a href="#">
+                            <div class="pet-imageitem" style="background-image: url(${buyitems[i].imagen});"></div>
+                        </a>
+                        <div class="petContenido marginbottom">
+                            <div class="petItemstext">
+                                <a class="anchor-label" href="#">${buyitems[i].producto}</a>
+                            </div>
+                            <div class="eligible">${buyitems[i].eligible}</div>
+                            <div class="petItemsprice margin-item">
+                                <p><span class="dolar">$</span><span class="number">${buyitems[i].numero}</span><span class="centavos">${buyitems[i].centavo}</span></p>
+                            </div> 
+                        </div>
+                    </div>
+                    `
+                    );
+            }else{
+                $("#petItems").append(
+                    `
+                    <div class="td-petitem col-xl-4 col-md-6 col-12">
+                        <a href="#">
+                            <div class="pet-imageitem" style="background-image: url(${buyitems[i].imagen});"></div>
+                        </a>
+                        <div class="petContenido">
+                            <div class="petItemstext">
+                                <a class="anchor-label" href="#">${buyitems[i].producto}</a>
+                            </div>
+                            <div class="petItemsprice margin-item">
+                                <p><span class="dolar">$</span><span class="number">${buyitems[i].numero}</span><span class="centavos">${buyitems[i].centavo}</span></p>
+                            </div>
+                        </div>
+                    </div>
+                    `
+                );
+            }
+        }
+    }
+}
